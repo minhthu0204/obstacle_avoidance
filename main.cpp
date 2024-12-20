678d64f1b71d72b79fcd1567e56a014bb4440923
minhthu0204 <phulinhnguyen3@gmail.com>
2024-12-20 10:01:02 +0700

    fourth

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     // Địa chỉ WebSocket server với hostname là 192.168.1.100 và port là 3335
-    QUrl serverUrl("ws://192.168.1.100:3335");  // Địa chỉ WebSocket server
-    WebSocketClient client(serverUrl);
+    //QUrl serverUrl("ws://192.168.1.100:3335");  // Địa chỉ WebSocket server
+    //WebSocketClient client(serverUrl);
 
     // Kết nối đến WebSocket server
-    client.connectToServer();
+    //client.connectToServer();
 
-    //DisplayManager displayManager;
-    //displayManager.run();
+    DisplayManager displayManager;
+    displayManager.run();
 
     // Lắng nghe các tin nhắn từ server
     // QObject::connect(&client, &WebSocketClient::messageReceived, [](const QString &message) {
